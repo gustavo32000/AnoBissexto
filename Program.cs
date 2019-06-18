5519d6dac20c6e3a12b2c1004bc8a4560be65f5b
gugu <gustavocorrea2032@gmail.com>
2019-06-18 11:54:28 -0300

    Calculo do ano bissexto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AnoBissexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int ano;
+
+          Console.WriteLine ("Digite um ano");
+            ano = int.Parse(Console.ReadLine());
+
+        if (ano % 4 != 0)
+            Console.WriteLine ("O ano não é bissexto!");
+        
+        else Console.WriteLine ("O ano é bissexto!");
+        
+          
+        }
+    }
+}
